4ea864b66d989d67197224f5ed7a76d605281d86
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2023-09-09 13:58:33 -0400

    added chat qna + more information

--- python/reviews.py
+++ python/reviews.py
@@ -21,36 +21,62 @@ def get_place_ids_from_latlon(lat, lon, radius='', type='', kwd=''):
     return ids
 
 
-def get_reviews_from_id(id):
-    ret_fields = ['name', 'reviews']
+def get_info_from_id(id):
+    ret_fields = ['name', 'delivery', 'dine_in', 'price_level', 'rating', 'reservable', 'serves_breakfast', 'serves_brunch', 'serves_dinner', 'serves_lunch', 'serves_vegetarian_food', 'takeout', 'reviews']
     params = {
         'place_id' : id,
         'key' : os.getenv('PENNAPPS_GOOGLE_API_KEY'),
         'fields' : ','.join(ret_fields),
         'reviews_sort' : 'most_relevant'
     }
-
     results = requests.get('https://maps.googleapis.com/maps/api/place/details/json', params=params).json()
 
     name = results['result']['name']
-    reviews = {name : []}
+    info = {}
+    reviews = []
     for rev in results['result']['reviews']:
         rating_review = {}
         rating_review['rating'] = rev['rating']
         rating_review['review'] = rev['text']
-        reviews[name].append(rating_review)
+        reviews.append(rating_review)
+    info['reviews'] = reviews
 
-    return reviews
+    # add other fields
+    for f in ret_fields[:-1]:
+        if f in results['result']:
+            f_new = f
+            if '_' in f:
+                f_new = ' '.join(f.split('_'))
+            info[f_new] = results['result'][f]
+
+    return name, info
 
 
+def qna(question, info_dict, model):
+    context = ''
+    for review in info_dict['reviews']:
+        context += review['review'] + ' '
 
-# qa_model = pipeline("question-answering")
-# question = "How is this restaurant's yogurt?"
-# context = "What a great place, you know you're in the right restaurant when they are non stop busy. We had a big group, got our salads/stir fry pretty fast. Be prepared to wait a bit as they seem to do a lot of take out/door dash orders. One of us had stir fry which came out very fast (super yummy) but the salads, which are made at the other end of the bar (this was confusing as to when our order was ready) but everything was very fresh. Love the touch screen ordering! Yikes, smallest portion $9 yogurt treat!! Don't get me wrong ... I love honeygrow and go often. Spent $70 for lunch and this is what I got. Was planning on sharing it ... ha.  Lil' disappointed. KOP location gives generously. This team really makes my day. I’m a regular here and sometimes when I come it isn’t just about the food … they smile, wave, if they aren’t super busy they’ll ask how I am. I love that. The service here is top tier. Place gets packed at lunch. Definitely download the app and pre-order during their lunch rush."
-# result = qa_model(question = question, context = context)
-# print(result['answer'])
+    other_info = ''
+    info_fields = set(['delivery', 'dine in', 'reservable', 'serves breakfast', 'serves brunch', 'serves dinner', 'serves lunch', 'serves vegetarian food', 'takeout'])
+    for field in info_fields:
+        if field in info_dict:
+            yes_no = "Yes"
+            if info_dict[field] == 'False':
+                yes_no = 'No'
+            other_info += yes_no + ' ' + field + '. '
+
+
+    context = '{}\n\n{}'.format(context, other_info)
+
+    result = model(question=question, context=context)
+    return result['answer']
 
 if __name__ == '__main__':
-    place_ids = get_place_ids_from_latlon(40.741482,-74.1827, radius=1000, type='restaurant')
-    reviews = get_reviews_from_id(place_ids[0])
-    print(reviews)
+    qna_model = pipeline("question-answering")
+    place_ids = get_place_ids_from_latlon(40.741482, -74.1827, radius=1000, type='restaurant')
+    name, info = get_info_from_id(place_ids[0])
+
+    print(qna('How are the smoothies?', info, qna_model))
+    print(qna('Do they serve vegetarian food?', info, qna_model))
+    print(qna('Do they serve breakfast?', info, qna_model))
